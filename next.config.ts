fb23e432a8ac957633f2ac90fd0584a0632ff24c
jean-blip <jeandorff@gmail.com>
2025-12-03 17:56:23 -0600

    Disable middleware + remove experimental flag

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-  experimental: {
-    middlewarePrefetch: "strict",
-  },
+  /* no experimental middleware settings */
 };
 
 export default nextConfig;
